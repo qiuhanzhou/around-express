b770ada731e65466b44e8d320ed3cbef3822f447
Qiuhan Zhou <qiuhanzhou@gmail.com>
2022-06-09 14:47:31 -0400

    build API

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const usersRouter = require('./routes/users');
+const cardRouter = require('./routes/card');
+
+const app = express();
+const { PORT = 3000 } = process.env;
+
+// to serve static files that are in the public directory ie. http://localhost:3000/kitten.jpg
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// continue on to use router that does not match public folder thru '/'
+app.use('/', usersRouter);
+app.use('/', cardRouter);
+
+// this matches all routes and all methods i.e a centralized error handler
+app.use((res) => {
+  res.status(404).send({
+    error: 'Requested resource not found',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
---
+++ routes/users.js
@@ -0,0 +1,70 @@
+// create the router
+const router = require('express').Router();
+
+// use the fs module to read data from this file when GET requests are made to the /users endpoint.
+const fsPromises = require('fs').promises;
+
+// create users path for reading
+const path = require('path');
+
+const USERS_PATH = path.join(__dirname, '../data/users.json');
+
+function readUsersFilePromise() {
+  return fsPromises.readFile(USERS_PATH, { encoding: 'utf8' });
+}
+
+function respondServerError(res) {
+  res.status(500).send({ message: 'An error has occured' });
+}
+
+function findUserById(users, id) {
+  const parsedUsersData = JSON.parse(users);
+  return parsedUsersData.find((user) => user._id === id);
+}
+
+// create users router to respond with all users data
+const sendUsers = (req, res) => {
+  // fsPromises
+  //   .readFile(USERS_PATH, { encoding: 'utf8' })
+  //   .then((users) => {
+  //     res.send(users);
+  //   })
+  //   .catch(() => res.status(500).send({ message: 'An error has occured' }));
+
+  readUsersFilePromise()
+    .then((users) => {
+      res.send(users);
+    })
+    .catch(() => respondServerError(res));
+};
+
+router.get('/users', sendUsers);
+
+// create dynamic user id router to respond matching user data
+
+const doesUserExist = (req, res, next) => {
+  readUsersFilePromise()
+    .then((users) => {
+      const user = findUserById(users, req.params.id);
+      if (!user) {
+        res.status(404).send({ message: 'User ID not found' });
+        return;
+      }
+      next();
+    })
+    // .catch(() => res.status(500).send({ message: 'An error has occured' }));
+    .catch(() => respondServerError(res));
+};
+
+const sendUser = (req, res) => {
+  readUsersFilePromise()
+    .then((users) => {
+      const user = findUserById(users, req.params.id);
+      res.send({ data: user });
+    })
+    .catch(() => respondServerError(res));
+};
+
+router.get('/users/:id', doesUserExist, sendUser);
+
+module.exports = router;
